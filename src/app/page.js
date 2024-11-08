b23a052eba84b727a0dad0f88b85b1de903b2a31
huzaifa8320 <muhammadhuzaifa8320@gmailcom>
2024-11-08 22:19:27 +0500

    Add Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
+       <Navbar/>
       <div className="bg-cover bg-center md:h-screen min-h-screen flex items-center" style={{ backgroundImage: "url('/Hospital blur bg.jpg')" }}>
         <HeroSection />
       </div>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,32 +2,26 @@ import Image from "next/image";
 
 export default function HeroSection() {
     return (
-        <div className="flex w-full h-full flex-col md:flex-row items-center justify-evenly p-3">
-            {/* Info Section */}
-            <div className="md:w-1/2 mb-8 md:mb-0 mt-8 text-center">
-                <h1 className="sm:text-4xl text-3xl font-bold text-gray-800 mb-4">
-                Your health journey starts here
-                </h1>
-                <p className="text-lg text-gray-600 mb-6 leading-relaxed">
-                Revolutionize your healthcare experience with our intuitive platform, making appointment booking as effortless as enjoying a cold-pressed juice. Connect with compassionate professionals for routine check-ups or specialized care, all just a click away. Take the first step towards a healthier, happier you today!
-
-
-                </p>
-                <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition duration-200">
-                    Shop Now
-                </button>
+        <section className="text-gray-600 container body-font mx-auto mt-28 md:mt-10 px-5 lg:px-10">
+            <div className="container flex md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"> Your health journey starts here </h1>
+                    <p className="mb-8 leading-relaxed">Revolutionize your healthcare experience with our intuitive platform, making appointment booking as effortless as enjoying a cold-pressed juice. Connect with compassionate professionals for routine check-ups or specialized care, all just a click away. Take the first step towards a healthier, happier you today!</p>
+                    <div className="flex flex-wrap sm:flex-nowrap gap-3">
+                        <button className="min-w-40 text-[#1F2937] p-2  border font-thi border-[#1F2937] shadow-md focus:outline-none rounded text-lg">Meet Your Doctor</button>
+                        <button className="min-w-40 text-white p-2 shadow-md focus:outline-none rounded text-lg bg-[#1F2937]">Apply as Doctor</button>
+                    </div>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex justify-center">
+                    <Image
+                        className="object-cover object-center rounded"
+                        alt="hero"
+                        height={400}
+                        width={400}
+                        src="/instant-img.avif"
+                    />
+                </div>
             </div>
-
-            {/* Image Section */}
-            <div className="h-full">
-                <Image
-                width={500}
-                height={500}
-                    src="/instant-img.avif" // Replace with your image URL
-                    alt="Hero"
-                    className="w-full h-full rounded-lg"
-                />
-            </div>
-        </div>
+        </section >
     );
 }
